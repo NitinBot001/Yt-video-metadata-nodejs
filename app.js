87d4792dbe58209bab799b7f8726b372f9960a35
NitinBot001 <112721096+NitinBot001@users.noreply.github.com>
2024-09-06 17:25:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get('/get_metadata', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+//const PORT = process.env.PORT || 3000;
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`);
 });
